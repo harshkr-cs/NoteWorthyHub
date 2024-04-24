9c606aee98330682a3e38d1c4bce3bcb85874857
Harsh Kumar <112871197+harshkr-cs@users.noreply.github.com>
2024-04-25 01:09:34 +0530

    config.js

--- src/config.js
+++ src/config.js
@@ -2,4 +2,4 @@ import { config } from "dotenv";
 config();
 
 export const PORT = process.env.PORT || 4000;
-export const MONGODB_URI = process.env.MONGODB_URI || "mongodb+srv://harshwwe555:3LgQpp423ZYvPYwq@cluster0.xnolxxq.mongodb.net/notesDatabase";
+export const MONGODB_URI = process.env.MONGODB_URI || MongoDBAtlas_URL;
